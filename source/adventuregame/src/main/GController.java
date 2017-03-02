dceed934c470d37e8719814e1ba0966c76f65567
Mike DiDomenico <md055@bucknell.edu>
2017-03-02 17:20:43 -0500

    started general structure

---
+++ source/adventuregame/src/main/GController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import game.Game;
+import view.View;
+
+/**
+ *
+ * @author Mike
+ */
+public class GController {
+
+    public Game game;
+    public View view;
+
+    public GController() {
+        this.game = new Game();
+        this.view = new View();
+
+    }
+
+}
---
+++ source/adventuregame/src/utility/Spatial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author Mike
+ */
+public class Spatial {
+
+    public double x;
+    public double y;
+    public double z;
+
+    public Spatial(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = x;
+    }
+
+}
